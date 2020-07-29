f44f76e8b8dd07fb56d8f6a7bd10d13d43906bc0
AdiletAldazharbekov <aldajarbekov@gmail.com>
2020-07-29 23:40:30 +0600

    Create abstract class Animal

---
+++ Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment13
+{
+    abstract class Animal
+    {
+        public string Name { get; }
+        public bool Gender { get; }
+
+        public int Age { get; }
+        public int Weight { get; }
+
+        public Animal()
+        {
+            Name = Name;
+            Age = Age;
+            Gender = Gender;
+            Weight = Weight;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("20");
+        }
+    }
+}
